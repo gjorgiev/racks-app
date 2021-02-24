7427705016e217d64a6076eb43e1de498b205761
Georgi Georgiev <gjorgi.hoho@gmail.com>
2021-02-24 20:43:20 +0100

    Fixed C1 drawing

--- src/components/DrawRoom.js
+++ src/components/DrawRoom.js
@@ -1,56 +1,59 @@
-import React, { useState } from 'react'
-import { Button, Form } from 'react-bootstrap'
+import React, { useState } from "react";
+import { Button, Form } from "react-bootstrap";
 
-const DrawRoom = ({onDraw}) => {
+const DrawRoom = ({ onDraw }) => {
+  const [A, setA] = useState("");
+  const [B, setB] = useState("");
+  const [C1, setC1] = useState("");
+  const [C2, setC2] = useState("");
 
-    const [A, setA] = useState('')
-    const [B, setB] = useState('')
-    const [C1, setC1] = useState('')
-    const [C2, setC2] = useState('')
+  const onSubmit = (e) => {
+    e.preventDefault();
 
-    const onSubmit = (e) => {
-        e.preventDefault()
+    onDraw({ A, B, C1, C2 });
+  };
 
-        onDraw({A, B, C1, C2})
-    }
+  return (
+    <Form onSubmit={onSubmit}>
+      <Form.Group>
+        <Form.Label>A</Form.Label>
+        <Form.Control
+          type="number"
+          placeholder="Enter Side A"
+          value={A}
+          onChange={(e) => setA(e.target.value)}
+        />
+      </Form.Group>
+      <Form.Group>
+        <Form.Label>B</Form.Label>
+        <Form.Control
+          type="number"
+          placeholder="Enter Side B"
+          value={B}
+          onChange={(e) => setB(e.target.value)}
+        />
+      </Form.Group>
+      <Form.Group>
+        <Form.Label>C1</Form.Label>
+        <Form.Control
+          type="number"
+          placeholder="Enter Side C1"
+          value={C1}
+          onChange={(e) => setC1(e.target.value)}
+        />
+      </Form.Group>
+      <Form.Group>
+        <Form.Label>C2</Form.Label>
+        <Form.Control
+          type="number"
+          placeholder="Enter Side C2"
+          value={C2}
+          onChange={(e) => setC2(e.target.value)}
+        />
+      </Form.Group>
+      <Button as="input" type="submit" value="Draw Room" />
+    </Form>
+  );
+};
 
-    return (
-        <Form onSubmit={onSubmit}>
-            <Form.Group>
-                <Form.Label>A</Form.Label>
-                <Form.Control
-                    type="number"
-                    placeholder="Enter Side A"
-                    value={A}
-                    onChange={(e) => setA(e.target.value)} />
-            </Form.Group>
-            <Form.Group>
-                <Form.Label>B</Form.Label>
-                <Form.Control
-                    type="number"
-                    placeholder="Enter Side A"
-                    value={B}
-                    onChange={(e) => setB(e.target.value)} />
-            </Form.Group>
-            <Form.Group>
-                <Form.Label>C1</Form.Label>
-                <Form.Control
-                    type="number"
-                    placeholder="Enter Side A"
-                    value={C1}
-                    onChange={(e) => setC1(e.target.value)} />
-            </Form.Group>
-            <Form.Group>
-                <Form.Label>C2</Form.Label>
-                <Form.Control
-                    type="number"
-                    placeholder="Enter Side A"
-                    value={C2}
-                    onChange={(e) => setC2(e.target.value)} />
-            </Form.Group>
-            <Button as='input' type='submit' value='Draw Room' />
-        </Form>
-    )
-}
-
-export default DrawRoom
+export default DrawRoom;
--- src/components/Walls.js
+++ src/components/Walls.js
@@ -1,39 +1,40 @@
-import React from 'react'
-import { Shape } from 'react-konva'
+import React from "react";
+import { Shape } from "react-konva";
 
-const Walls = ({data, margin}) => {
+const Walls = ({ data, margin }) => {
+  const { A, B, C1, C2 } = data;
 
-    const {A, B, C1, C2} = data
+  return (
+    <Shape
+      sceneFunc={(context, shape) => {
+        context.beginPath();
+        // start drawing the walls
+        // set the starting point to draw the first wall A
+        context.moveTo(margin, margin);
+        context.lineTo(A, margin);
+        // let's draw the wall B1
+        context.moveTo(margin, margin);
+        context.lineTo(margin, B);
+        // draw wall B2
+        context.moveTo(A, margin);
+        context.lineTo(A, B);
+        // draw wall C1
 
-    return (
-        <Shape
-            sceneFunc={(context, shape) => {
-                context.beginPath();
-                // start drawing the walls
-                // set the starting point to draw the first wall A
-                context.moveTo(margin, margin)
-                context.lineTo(A, margin)
-                // let's draw the wall B1
-                context.moveTo(margin, margin)
-                context.lineTo(margin, B)
-                // draw wall B2
-                context.moveTo(A, margin)
-                context.lineTo(A, B)
-                // draw wall C1
-                context.moveTo(margin, B)
-                context.lineTo(C1, B)
-                // draw wall C2
-                context.moveTo(A-C2, B)
-                context.lineTo(A, B)
-                // execute the drawing
-                context.closePath()
-                // (!) Konva specific method, it is very important
-                context.fillStrokeShape(shape);
-            }}
-            strokeWidth={3}
-            stroke="black"
-        />
-    )
-}
+        context.moveTo(margin, B);
+        context.lineTo(parseInt(C1) + parseInt(margin), B);
 
-export default Walls
+        // draw wall C2
+        context.moveTo(A - C2, B);
+        context.lineTo(A, B);
+        // execute the drawing
+        context.closePath();
+        // (!) Konva specific method, it is very important
+        context.fillStrokeShape(shape);
+      }}
+      strokeWidth={3}
+      stroke="black"
+    />
+  );
+};
+
+export default Walls;
